77bb0203a7eda2db8a98debde4b08836ee9d4b28
Guangyu He <guangyu.he@golden-tech.de>
2026-01-25 19:54:09 +0100

    add axum integration for Python function execution and HTTP response handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 
-fn main() -> PyResult<()> {
-    // Example of initializing Python interpreter and calling a Python function
-    Python::initialize();
-    Python::attach(|py| {
+use axum::response::IntoResponse;
+use axum::{Router, routing::get};
+
+async fn py_example() -> impl IntoResponse {
+    let result: PyResult<String> = Python::attach(|py| {
         let sys = py.import("sys")?;
         let path = sys.getattr("path")?;
-        path.call_method1("append", (".venv/lib/python3.13/site-packages",))?; // append my venv path
-        path.call_method1("append", ("./python",))?; // append my custom module path
+        path.call_method1("append", (".venv/lib/python3.13/site-packages",))?;
+        path.call_method1("append", ("./python",))?;
 
         let module = py.import("main")?;
         let user_cls = module.getattr("User")?;
 
-        // 构造参数（dict / kwargs）
         let kwargs = PyDict::new(py);
         kwargs.set_item("id", 1)?;
         kwargs.set_item("name", "Bob")?;
@@ -21,10 +21,24 @@ fn main() -> PyResult<()> {
 
         let user = user_cls.call((), Some(&kwargs))?;
 
-        // 调用方法
-        let greet: String = user.getattr("greet")?.call0()?.extract()?;
-        println!("{}", greet);
+        let s: String = user.getattr("greet")?.call0()?.extract()?;
+        Ok(s)
+    });
 
-        Ok(())
-    })
+    match result {
+        Ok(s) => s.into_response(),
+        Err(e) => format!("python error: {e}").into_response(),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(py_example));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("Listening on http://127.0.0.1:3000");
+    axum::serve(listener, app).await.unwrap();
 }
